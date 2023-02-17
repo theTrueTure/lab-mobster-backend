aec354f4d2e4ce2f63ef6a92d8de13d07c7c7c29
Feng Y <fengy142857@gmail.com>
2023-02-17 09:21:38 +0100

    add members data

--- routes/db.js
+++ routes/db.js
@@ -10,13 +10,22 @@ let mobs = [
 ];
 let members = [
 	{
-		names: ['Feng', 'Simon', 'Senne', 'Mariana'],
-		mobId: 1676464991028,
-	},
-	{
-		names: ['a', 'b', 'c', 'd'],
-		mobId: 1,
-	},
+		mobID: 1676464991028,
+		memberId: 1,
+		name: "Feng",
+	},{
+		mobID: 1676464991028,
+		memberId: 2,
+		name: "Mariana",
+	},{
+		mobID: 1676464991028,
+		memberId: 3,
+		name: "Senna",
+	},{
+		mobID: 1676464991028,
+		memberId: 4,
+		name: "Simon",
+	}
 ];
 
 module.exports = { mobs, members };
